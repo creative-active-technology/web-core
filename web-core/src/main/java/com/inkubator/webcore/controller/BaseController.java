8013bd0957754146ff95cb71407d5a50a695435b
Deni Husni FR <Deni Husni FR@Positron>
2014-02-21 13:24:24 +0700

    Add HRM Entity, Dao and Service

--- web-core/src/main/java/com/inkubator/webcore/controller/BaseController.java
+++ web-core/src/main/java/com/inkubator/webcore/controller/BaseController.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.inkubator.webcore.controller;
 
+import com.inkubator.webcore.util.FacesUtil;
 import java.io.Serializable;
+import java.util.Locale;
+import javax.annotation.PostConstruct;
 import org.apache.log4j.Logger;
 
 /**
@@ -15,7 +17,25 @@ import org.apache.log4j.Logger;
  * @version 1
  * @since 10 February 2014
  */
-public abstract class BaseController implements Serializable{
-     protected transient Logger logger = Logger.getLogger(getClass());
-    
+public abstract class BaseController implements Serializable {
+
+    protected transient Logger LOGGER = Logger.getLogger(getClass());
+    protected String bahasa;
+
+    public String getBahasa() {
+        return bahasa;
+    }
+
+    public void setBahasa(String bahasa) {
+        this.bahasa = bahasa;
+    }
+
+    @PostConstruct
+    public void initialization() {
+        LOGGER.info("Base Controlller Execute");
+        bahasa = (String) FacesUtil.getSessionAttribute("bahasa_active");
+        FacesUtil.getFacesContext().getViewRoot().setLocale(new Locale(bahasa));
+        
+    }
+
 }
